9d3c4fca70c9af3f6052fa34b178823f18c20a9b
Mmy26 <yu.b1994masamura@gmail.com>
2024-06-25 15:02:13 +0900

    useEffect （条件付きレンダリング）

--- src/components/BasicUseEffect.js
+++ src/components/BasicUseEffect.js
@@ -3,14 +3,16 @@ import React, { useEffect, useState } from 'react'
 const BasicUseEffect = () => {
 
   const [count, setCount] = useState(0)
+  const [item, setItem] = useState("")
 
   useEffect(() => {
     console.log("useEffect invoked")
-  }, [])
+  },[count])
 
   return (
     <>
       <button onClick={()=>setCount(prevCount => prevCount + 1)}>Click { count }</button>
+      <input type="text" value={item} onChange={evt=>setItem(evt.target.value)} />
     </>
   )
 }
