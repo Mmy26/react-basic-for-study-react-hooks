b0171c2d81a80abc0148a1417b1ef6701f99a3c2
Mmy26 <yu.b1994masamura@gmail.com>
2024-06-25 15:43:18 +0900

    APIデータ取得(axios, fetch)

---
+++ src/components/ApiFetch.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+// import axios from 'axios'
+
+const ApiFetch = () => {
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    // axios.get('https://jsonplaceholder.typicode.com/posts')
+    // .then(res => {
+    //   setPosts(res.data)
+    // })
+    fetch('https://jsonplaceholder.typicode.com/posts', {method: 'GET'})
+      .then((res) => res.json())
+      .then(data => {
+        setPosts(data)
+      })
+  }, [])
+  return (
+    <>
+      <ul>
+        {
+          posts.map((post)=> <li key={ post.id }> { post.title } </li>)
+        }
+      </ul>
+    </>
+  )
+}
+
+export default ApiFetch
